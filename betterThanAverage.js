1df9a3dbc2b832b30711c18394103a9154e45cce
JamesCunningham304 <86136067+JamesCunningham304@users.noreply.github.com>
2022-04-13 15:05:56 -0700

    Add files via upload

---
+++ betterThanAverage.js
@@ -0,0 +1,11 @@
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+function betterThanAverage(classPoints, yourPoints) {
+    const sum = classPoints.reduce((a, b) => a + b, 0)
+    return yourPoints > (sum/classPoints.length)
+   }
