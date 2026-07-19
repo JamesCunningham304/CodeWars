17a3e099c3cb15ef034ebf6433f44b344a8fc1c4
James <86136067+JamesCunningham304@users.noreply.github.com>
2026-07-19 15:29:16 -0700

    Implement square function in squaring_an_argument
    Square a integer using a function kata.

---
+++ squaring_an_arguement.java
@@ -0,0 +1,9 @@
+//Now you have to write a function that takes an argument and returns the square of it.
+
+public class squaring_an_argument {
+    public static class Kata {
+        public static int square(int n) {
+            return n*n;
+        }
+    }
+}
